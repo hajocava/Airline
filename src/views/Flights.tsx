95f89e4689a3c1ade0d1afb36d6db40d98fd6632
Haziel <hajocava@gmail.com>
2021-12-06 16:58:32 -0600

    Start select seats view

--- src/views/Flights.tsx
+++ src/views/Flights.tsx
@@ -1,8 +1,7 @@
+import { SelectSeats } from "../components/selectSeats/SelectSeats"
 import { Wizard } from "../components/stepsWizzard/Wizard"
 import { Tickets } from "../components/tickets/Tickets"
 
-const Page2 = () => <h1>Page 2</h1>
-const Page3 = () => <h1>Page 3</h1>
 
 export const Flights = () => {
     return (
@@ -10,8 +9,7 @@ export const Flights = () => {
             <Wizard>
                 <Wizard.Pages>
                     <Tickets />
-                    <Page2 />
-                    <Page3 />
+                    <SelectSeats />
                 </Wizard.Pages>
                 <Wizard.ButtonPrev classNames="btn secondary" />
                 <Wizard.ButtonNext classNames="btn primary" />
